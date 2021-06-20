b745021c4d65985c03f63563fb630bf1dd374a25
psyonix-engg <67711111+psyonix-engg@users.noreply.github.com>
2021-06-20 13:16:55 +0300

    Create ban.js

---
+++ commands/moderation/ban.js
@@ -0,0 +1,65 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const BoltyMod = require("../../classes/BoltyMod");
+
+/**
+ *
+ * @param {Client} client
+ * @param {Message} message
+ * @param {String[]} args
+ */
+
+module.exports.run = async (client, message, args) => {
+  if (
+    !message.member.permissions.has("BAN_MEMBERS") ||
+    !message.member.permissions.has("ADMINISTRATOR")
+  )
+    return;
+
+  const target =
+    message.mentions.members.first() ||
+    message.guild.members.cache.get(args[0]);
+
+  const { guild } = message;
+
+  if (!target) {
+    BoltyMod.BoltyEmbed(client).setDescription(
+      `${BoltyMod.BoltyEmotes.wrong_error} you did not provide any member to ban, please mention the user or provide their ID.`
+    );
+  }
+
+  if (target) {
+    const member = guild.members.cache.get(target.id);
+
+    const reason = args.slice(1).join(" ");
+
+    if (reason === undefined) reason = "No reaosn was provided.";
+
+    if (member.bannable) {
+      member.ban({ reason: reason, days: 0 });
+      message.channel.send(
+        BoltyMod.BoltyKickEmbed(message)
+          .setAuthor(
+            `${member.user.tag} was banned`,
+            `https://cdn.discordapp.com/emojis/801791545060884510.png?v=1`
+          )
+          .addField(`Kicked By:`, `\`${message.author.tag}\``)
+          .addField(`Reason:`, `\`${reason}\``)
+          .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      );
+    } else {
+      message.channel.send(
+        BoltyMod.BoltyEmbed(client).setDescription(
+          `${BoltyMod.BoltyEmotes.wrong_error} I cannot ban that user.`
+        )
+      );
+    }
+  }
+};
+
+module.exports.help = {
+  name: "ban",
+  description: "Ban someone from the server.",
+  aliases: ["b"],
+  usage: "[user]",
+  category: "Moderation",
+};
