5830183282e6218d0fea3e4387321da5acfd9489
psyonix-engg <67711111+psyonix-engg@users.noreply.github.com>
2021-06-23 09:01:52 +0300

    x

---
+++ commands/fun/roll.js
@@ -0,0 +1,47 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const BoltyFun = require("../../classes/BoltyFun");
+
+/**
+ *
+ * @param {Client} client
+ * @param {Message} message
+ * @param {String[]} args
+ */
+
+module.exports.run = async (client, message, args) => {
+  let number = args.slice(1).join(" ");
+
+  if (isNaN(number))
+    message.channel.send(
+      BoltyFun.BoltyFunEmbed(client)
+        .setDescription(
+          `That was **not** a valid number, please provide a valid number.`
+        )
+        .setAuthor(
+          `${BoltyFun.BoltyEmotes.wrong_error} Error`,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+    );
+
+  if (!number) number = 6;
+
+  let n = Math.floor(Math.random() * number + 1);
+
+  message.channel.send(
+    BoltyFun.BoltyFunEmbed(client)
+      .setDescription(`${message.author}, you rolled **${n}**!`)
+      .setFooter(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+  );
+};
+
+module.exports.help = {
+  name: "roll",
+  description:
+    "Rolls a dice wit hthe specified number of sides. Will default to 6 sides if no number is given.",
+  aliases: ["dice", "r"],
+  usage: "[number]",
+  category: "Fun",
+};
---
+++ commands/fun/wouldyourather.js
@@ -0,0 +1,34 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { wyrQuestions } = require("../../assets/json/wouldyourather.json");
+const BoltyFun = require("../../classes/BoltyFun");
+
+/**
+ *
+ * @param {Client} client
+ * @param {Message} message
+ * @param {String[]} args
+ */
+
+module.exports.run = async (client, message, args) => {
+  const wyrQuestion =
+    wyrQuestions[Math.round(Math.random() * wyrQuestions.length)];
+
+  message.channel
+    .send(
+      BoltyFun.BoltyFunEmbed(client)
+        .setTitle(`**Would You Rather?**`)
+        .setDescription(`**${wyrQuestion}**`)
+    )
+    .then((msg) => {
+      msg.react("856895358897881088");
+      msg.react("856125687567613973");
+    });
+};
+
+module.exports.help = {
+  name: "would-you-rather",
+  description: "Would-You-Rather?",
+  aliases: ["wouldyourather", "wyr", "wur"],
+  usage: "",
+  category: "Fun",
+};
